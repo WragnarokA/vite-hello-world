919bf74a9902b709af276a976c6e3345a36d3251
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-22 02:02:52 +0200

    first commit

---
+++ il-mio-progettto/src/App.vue
@@ -0,0 +1,31 @@
+<script >
+import AppTitle from './components/AppTitle.vue'
+
+export default {
+  components: {
+    AppTitle,
+  }
+}
+
+
+</script>
+
+
+<template>
+  <main>
+
+    <AppTitle />
+
+  </main>
+</template>
+
+<style scoped>
+div {
+  width: 100vw;
+  height: 100vh;
+  background-color: #222;
+  padding: 0 auto;
+}
+</style>
+
+<style scoped></style>
---
+++ il-mio-progettto/src/components/AppTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="titolo">
+
+        <h1>La mia prima app con Vite!</h1>
+    </div>
+</template>
+
